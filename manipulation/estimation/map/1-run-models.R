56f6635972f53535f7898267c678f55fa6323397
Andriy V. Koval <koval.andrey@gmail.com>
2019-02-22 10:47:13 -0500

    Update 1-run-models.R

--- manipulation/estimation/map/1-run-models.R
+++ manipulation/estimation/map/1-run-models.R
@@ -94,10 +94,10 @@ ls_subgroup = list(
   # ,"unisex" = "unisex"
 )
 ls_model_type <- list(
-  # "a"       = c("age_c70")
+  "a"       = c("age_c70")
   # ,"ae"      = c("age_c70","edu_c7")
   # ,"aeh"     = c("age_c70","edu_c7","htm_c")
-  "aehplus" = c("age_c70","edu_c7","htm_c", "smoke","cardio","diabetes")
+  ,"aehplus" = c("age_c70","edu_c7","htm_c", "smoke","cardio","diabetes")
   # ,"aeplus" = c("age_c70","edu_c7", "smoke","stroke","diabetes")
 )
 
