1fb79fba47756865db66567085567ae5cf70db35
Andriy V. Koval <koval.andrey@gmail.com>
2019-03-20 12:02:43 -0400

    remove names

--- manipulation/estimation/map/1-run-models.R
+++ manipulation/estimation/map/1-run-models.R
@@ -100,8 +100,8 @@ ls_model_type <- list(
   ,"aehplus" = c("age_c70","edu_c7","htm_c", "smoke","cardio","diabetes")
   # ,"aeplus" = c("age_c70","edu_c7", "smoke","stroke","diabetes")
 )
-# joey 
-# andriy
+
+
 ############################################################ GRIP #####
 ## @knitr dummy_1
 # Use the first example as the template for further pairs
