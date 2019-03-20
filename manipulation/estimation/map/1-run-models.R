82d7ec785feca4d1bbd3c0c8ceeffd0fa7e88452
Joey <gjmerrin@gmail.com>
2019-03-20 11:11:13 -0400

    Update 1-run-models.R

--- manipulation/estimation/map/1-run-models.R
+++ manipulation/estimation/map/1-run-models.R
@@ -98,7 +98,7 @@ ls_model_type <- list(
   # ,"ae"      = c("age_c70","edu_c7")
   "aeh"     = c("age_c70","edu_c7","htm_c")
   # ,"aehplus" = c("age_c70","edu_c7","htm_c", "smoke","cardio","diabetes")
-  # ,"aeplus" = c("age_c70","edu_c7", "smoke","stroke","diabetes")
+  ,"aeplus" = c("age_c70","edu_c7", "smoke","stroke","diabetes")
 )
 
 
